618ce71591233873d082070cdbc6cf3f5c68a1b0
sunxiaobo <sunxiaobo.cn@gmail.com>
2016-10-23 00:21:10 +0800

    build at local, send to gh-pages 2016-10-23 00:21:10

---
+++ vendors/boot/index.js
@@ -0,0 +1,8 @@
+(function() {
+  if (location.hostname === 'sunxiaobo.com') {
+    window.stop() 
+    location.replace(location.href.replace('sunxiaobo.com', 'www.sunxiaobo.com'))
+    // 隐藏所有元素
+    document.write("<style>* {display: none;}</style>")
+  }
+})()
