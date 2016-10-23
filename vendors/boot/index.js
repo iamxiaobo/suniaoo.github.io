8c1ca19181e3d1ab48c52b217cc02c969c98d88e
sunxiaobo <sunxiaobo.cn@gmail.com>
2016-10-23 21:55:45 +0800

    build at local, send to gh-pages 2016-10-23 21:55:45

--- vendors/boot/index.js
+++ vendors/boot/index.js
@@ -1,9 +1,8 @@
-(function () {
-  if (location.hostname === 'sunxiaobo.com') {
-    window.stop()
-    location.replace(location.href.replace('sunxiaobo.com',
-        'www.sunxiaobo.com'))
-      // 隐藏所有元素
+(function() {
+  if (location.hostname !== 'sunxiaobo.com' && location.hostname !== 'localhost') {
+    window.stop() 
+    location.replace(location.href.replace(location.hostname, 'www.sunxiaobo.com'))
+    // 隐藏所有元素
     document.write("<style>* {display: none;}</style>")
   }
 })()
